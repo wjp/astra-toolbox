12ee366b5f2ae8ab18ce6156cce79e2a37a691bf
Willem Jan Palenstijn <w.j.palenstijn@liacs.leidenuniv.nl>
2024-08-01 11:15:18 +0200

    Adjust to number of GPUs dynamically

--- tests/python/test_multigpu_consistency.py
+++ tests/python/test_multigpu_consistency.py
@@ -5,10 +5,11 @@ def print_diff(a, b):
     x = np.abs((a-b).reshape(-1))
     print(np.linalg.norm(x, ord=2), np.max(x), np.max(np.abs(a)), np.max(np.abs(b)))
 
+N = 1024
 
-angles = np.linspace(0, 2*np.pi, 1024, endpoint=False)
-vg = astra.create_vol_geom(1024, 1024, 1024)
-pg = astra.create_proj_geom('cone', 1.0, 1.0, 1024, 1024, angles, 10000, 0)
+angles = np.linspace(0, 2*np.pi, N, endpoint=False)
+vg = astra.create_vol_geom(N, N, N)
+pg = astra.create_proj_geom('cone', 1.0, 1.0, N, N, angles, 10*N, 0)
 
 projector_id = astra.create_projector('cuda3d', pg, vg)
 W = astra.OpTomo(projector_id)
@@ -22,9 +23,21 @@ astra.data3d.delete(pid)
 
 rec_single = W.reconstruct('FDK_CUDA', projdata_single)
 
-for m in ( 0, 100000000 ):
-  for x in ( [0,1], [0,1,2], [0,1,2,3] ):
-    astra.set_gpu_index(x, memory=m)
+gpu = 0
+gpus = [0]
+
+while True:
+
+  gpu = gpu + 1
+  if 'Invalid' in astra.get_gpu_info(gpu):
+      print(f"No GPU #%s. Aborting." % (gpu,))
+      break
+
+  gpus.append(gpu)
+  print("Now using GPUs " + ", ".join(str(i) for i in gpus))
+
+  for m in ( 0, 100000000 ):
+    astra.set_gpu_index(gpus, memory=m)
 
     pid, projdata_multi = astra.create_sino3d_gpu(phantom_id, pg, vg, returnData=True)
     astra.data3d.delete(pid)
