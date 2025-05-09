8f2e467259f11629e76cbc7b90227d8be6d775c3
Willem Jan Palenstijn <w.j.palenstijn@liacs.leidenuniv.nl>
2025-05-09 16:04:25 +0200

    Exclude CUDA libraries from wheels for PyPI

--- build/pypi/build.sh
+++ build/pypi/build.sh
@@ -17,30 +17,30 @@ cd astra-toolbox/build/linux
 
 ./configure --with-python=python3.9 --with-cuda=/usr/local/cuda-12.1 --with-install-type=module
 make -j 20 ASTRA_CONFIG=pypi_linux_cuda all
-auditwheel repair --plat manylinux2014_x86_64 python/dist/*.whl
+auditwheel repair --plat manylinux2014_x86_64 --exclude "libcudart.so.*" --exclude "libcufft.so.*" python/dist/*.whl
 mv wheelhouse/*.whl /out
 make clean
 
 ./configure --with-python=python3.10 --with-cuda=/usr/local/cuda-12.1 --with-install-type=module
 make -j 20 ASTRA_CONFIG=pypi_linux_cuda all
-auditwheel repair --plat manylinux2014_x86_64 python/dist/*.whl
+auditwheel repair --plat manylinux2014_x86_64 --exclude "libcudart.so.*" --exclude "libcufft.so.*" python/dist/*.whl
 mv wheelhouse/*.whl /out
 make clean
 
 ./configure --with-python=python3.11 --with-cuda=/usr/local/cuda-12.1 --with-install-type=module
 make -j 20 ASTRA_CONFIG=pypi_linux_cuda all
-auditwheel repair --plat manylinux2014_x86_64 python/dist/*.whl
+auditwheel repair --plat manylinux2014_x86_64 --exclude "libcudart.so.*" --exclude "libcufft.so.*" python/dist/*.whl
 mv wheelhouse/*.whl /out
 make clean
 
 ./configure --with-python=python3.12 --with-cuda=/usr/local/cuda-12.1 --with-install-type=module
 make -j 20 ASTRA_CONFIG=pypi_linux_cuda all
-auditwheel repair --plat manylinux2014_x86_64 python/dist/*.whl
+auditwheel repair --plat manylinux2014_x86_64 --exclude "libcudart.so.*" --exclude "libcufft.so.*" python/dist/*.whl
 mv wheelhouse/*.whl /out
 make clean
 
 ./configure --with-python=python3.13 --with-cuda=/usr/local/cuda-12.1 --with-install-type=module
 make  -j 20 ASTRA_CONFIG=pypi_linux_cuda all
-auditwheel repair --plat manylinux2014_x86_64 python/dist/*.whl
+auditwheel repair --plat manylinux2014_x86_64 --exclude "libcudart.so.*" --exclude "libcufft.so.*" python/dist/*.whl
 mv wheelhouse/*.whl /out
 make clean
