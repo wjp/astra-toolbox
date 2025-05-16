524efa9f04cf3d9d208b488e597ca00ef8c0f6e0
Willem Jan Palenstijn <w.j.palenstijn@liacs.leidenuniv.nl>
2025-05-16 15:39:20 +0200

    Fix pip install call

--- build/conda/astra-toolbox/bld.bat
+++ build/conda/astra-toolbox/bld.bat
@@ -9,4 +9,4 @@ cd python
 set CL=/DASTRA_CUDA /DASTRA_PYTHON "/I%R%\include" "/I%R%\lib\include" "/I%CUDA_PATH%\include" /std:c++17
 set ASTRA_CONFIG=conda_cuda
 copy "%LIBRARY_LIB%\AstraCuda64.lib" astra.lib
-python -m pip install
+python -m pip install .
